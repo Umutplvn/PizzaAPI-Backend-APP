f75bf51fac7647131d8671875eaa9a1d7ee5af60
umutplvn <umutpehlivan2078@gmail.com>
2023-10-16 04:05:40 +0100

    order

--- index.js
+++ index.js
@@ -54,12 +54,18 @@ app.all('/', (req, res)=>{
     })
 })
 
-//user:
-app.use('/users', require('./src/routes/user'))
 
-//topping:
+
+// user:
+app.use('/users', require('./src/routes/user'))
+// order:
+app.use('/orders', require('./src/routes/order'))
+// pizza:
+app.use('/pizzas', require('./src/routes/pizza'))
+// topping:
 app.use('/toppings', require('./src/routes/topping'))
 
+/* ------------------------------------------------------- */
 
 
 /* ------------------------------------------------------- */
--- src/models/pizza.js
+++ src/models/pizza.js
@@ -4,3 +4,38 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+// Pizza Model:
+
+const PizzaSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+
+    image: {
+        type: String,
+        trim: true,
+    },
+
+    price: {
+        type: Number,
+        required: true,
+    },
+
+    toppings: [ // push, pull   // birden fazla topping veri girisi olabilecegi icin array icine koyuyoruz ve bunlari push pull komutlari ile yonetebiliyoruz
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Topping'  // iliski kurulacak olan model
+        }
+    ]
+
+}, {
+    collection: 'pizzas',
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Pizza', PizzaSchema)
